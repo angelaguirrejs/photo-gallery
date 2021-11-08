e322aa0eb13c8a746e0e75c73ca88a8f9d89f889
Angel Aguirre <angelaguirre.personal@gmail.com>
2021-11-08 15:06:12 -0600

    View to see photo and view to upload photos have been created

---
+++ views/photo.view.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galería</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="https://kit.fontawesome.com/780d1b883d.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1 class="title">Foto: 1.jpg</h1>
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="photo">
+            <img src="https://images.pexels.com/photos/9461022/pexels-photo-9461022.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+            <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dicta eos saepe alias itaque quibusdam sed in minima nemo doloremque et molestias reiciendis unde, iste, quam ad necessitatibus adipisci corrupti.</p>
+            <a href="index.php" class="back"><i class="fa fa-long-arrow-left"></i> Regresar</a>
+        </div>
+    </div>
+
+    <footer>
+        <p class="copyright">Galería creada por Angel Aguirre</p>
+    </footer>
+
+</body>
+</html>
---
+++ views/upload.view.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galería</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="https://kit.fontawesome.com/780d1b883d.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1 class="title">Subir fotos</h1>
+        </div>
+    </header>
+
+    <div class="container">
+        <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST" enctype="multipart/form-data" class="form">
+            <label for="photo">Selecciona la foto</label>
+            <input type="file" id="photo" name="photo">
+
+            <label for="title">Título de la foto</label>
+            <input type="text" id="title" name="title"  placeholder="Título">
+
+            <label for="text">Descripción</label>
+            <textarea name="description" id="text" cols="30" rows="10" placeholder="Descripción"></textarea>
+
+            <input type="submit" class="submit" value="Subir foto">
+        </form>
+    </div>
+
+    <footer>
+        <p class="copyright">Galería creada por Angel Aguirre</p>
+    </footer>
+
+</body>
+</html>
