742f720c81c0463b7d7dd9cc49d0e15ed8b5b8f9
Angel Aguirre <angelaguirre.personal@gmail.com>
2021-11-08 14:12:50 -0600

    Homepage created and styles added

---
+++ views/index.view.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galería</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="https://kit.fontawesome.com/780d1b883d.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1 class="title">Galería de Imagenes</h1>
+        </div>
+    </header>
+
+    <section class="photos">
+        <div class="container">
+            <div class="thumb">
+                <a href="">
+                    <img src="https://images.pexels.com/photos/9461022/pexels-photo-9461022.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="">
+                    <img src="https://images.pexels.com/photos/9461022/pexels-photo-9461022.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="">
+                    <img src="https://images.pexels.com/photos/9461022/pexels-photo-9461022.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="">
+                    <img src="https://images.pexels.com/photos/9461022/pexels-photo-9461022.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="">
+                    <img src="https://images.pexels.com/photos/9461022/pexels-photo-9461022.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="">
+                    <img src="https://images.pexels.com/photos/9461022/pexels-photo-9461022.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="">
+                    <img src="https://images.pexels.com/photos/9461022/pexels-photo-9461022.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="">
+                    <img src="https://images.pexels.com/photos/9461022/pexels-photo-9461022.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="">
+                    <img src="https://images.pexels.com/photos/9461022/pexels-photo-9461022.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                </a>
+            </div>
+        
+            <div class="pagination">
+                <a href="#" class="left"><i class="fa fa-long-arrow-left"></i> Página Anterior</a>
+                <a href="#" class="right">Página Siguiente <i class="fa fa-long-arrow-right"></i></a>
+            </div> 
+        </div>
+    </section>
+
+    <footer>
+        <p class="copyright">Galería creada por Angel Aguirre</p>
+    </footer>
+
+</body>
+</html>
